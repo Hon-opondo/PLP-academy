650167e89fad5fbae906e0bb30b07b228f04e771
Hongo <93112823+Hon-opondo@users.noreply.github.com>
2025-04-02 12:42:19 +0300

    Add files via upload
    understanding in-built data structures in python

---
+++ data structures.py
@@ -0,0 +1,26 @@
+#create an empty list
+my_list = []
+
+#appending values to a list
+my_list.append(10)
+
+my_list.append(20)
+
+my_list.append(30)
+
+my_list.append(40)
+
+#insert a value at a particular index
+my_list.insert(2, 15)
+
+#extending the list with another list
+my_list.extend([50, 60, 70])
+
+#remove the last element in the list
+my_list.pop(-1)
+
+#sorting in ascending order
+my_list.sort()
+
+#finding the index of a particular value
+print(my_list.index[30])
